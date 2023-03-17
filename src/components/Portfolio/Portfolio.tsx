f2ab1a0eaae360d834d016ad9d99c0d8de13ec61
Seb <rush@Sebs-MacBook-Air.local>
2023-03-17 12:35:11 +0100

    Updated portfolio css and comonent

--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -1,17 +1,48 @@
 import React from 'react';
 
-import { Container } from 'semantic-ui-react';
+import './Portfolio.scss';
 
 const Portfolio: React.FC = () => {
 
     return (
-        <Container className='content'>
-            <div className='header'>
+        <div className='portfolio__container'>
+            <div className='portfolio__header'>
                 <h1>Portfolio</h1>
             </div>
-            <div className='body'>
+            <div className='portfolio__content'>
+
+                <div className='item-a'>
+                    <h1>Info</h1>
+                </div>
+                <div className='item-b'>
+                    <h1>Bot info</h1>
+                </div>
+
+                <div className='item-c'>
+                    <h1>Order history</h1>
+                    <table cellSpacing={0}>
+                        <thead>
+                            <tr>
+                                <th>
+
+                                </th>
+                                <th>
+                                    Date
+                                </th>
+                            </tr>
+                        </thead>
+
+                        <tbody>
+                            <tr>
+                                <td>
+                                    2021-01-01
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
             </div>
-        </Container>
+        </div>
     );
 }
 
